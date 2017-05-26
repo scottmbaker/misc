a1df6b1e96796e190023fe213a60d92205b1c5fd
Scott Baker <smbaker@gmail.com>
2017-05-26 10:01:43 -0700

    conditional setup

--- xos/setup.sh
+++ xos/setup.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 cd ~
-git clone https://github.com/sbconsulting/misc.git scott-misc
+if [ -e scott-misc ]; then
+    cd scott-misc
+    git pull
+    cd ..
+else
+    git clone https://github.com/sbconsulting/misc.git scott-misc
+fi
 cd scott-misc/xos
 cp teardown.sh bringup.sh /opt/cord/build/platform-install/scripts/
