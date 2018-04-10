f1f8392ae2f4ef4d55ad2eb9d0db47364caaec80
Scott Baker <smbaker@gmail.com>
2018-04-10 13:54:09 -0700

    new aliases

--- xos/setup.sh
+++ xos/setup.sh
@@ -8,8 +8,8 @@ else
     git clone https://github.com/sbconsulting/misc.git scott-misc
 fi
 cd scott-misc/xos
-cp aliases.sh teardown.sh bringup.sh /opt/cord/build/platform-install/scripts/
-sudo chmod a+r /opt/credentials/cord_keystone_admin
+mkdir -p /opt/smbaker
+cp aliases.sh teardown.sh bringup.sh /opt/smbaker/
 source aliases.sh
-echo "source /opt/cord/build/platform-install/scripts/aliases.sh" >> ~/.bashrc
-cd /opt/cord/build/platform-install
+echo "source /opt/smbaker/aliases.sh" >> ~/.bashrc
+
