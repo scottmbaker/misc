25fd4e8a2b7561e61408d12408f451ba6acae99f
Scott Baker <smbaker@gmail.com>
2018-04-16 17:09:31 -0700

    helm-aliases

--- xos/aliases.sh
+++ xos/aliases.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
 alias helm-cycle="/opt/smbaker/helm-cycle.sh"
+alias helm-xossh-start="kubectl get pods | grep -i xossh | grep -v "erminating" > /dev/null || (pushd ~/cord/build/helm-charts; helm install xos-tools/xossh -n xossh; popd)"
+alias helm-xossh-stop="helm del --purge xossh"
+alias helm-xossh="~/cord/build/helm-charts/xos-tools/xossh/xossh-attach.sh"
