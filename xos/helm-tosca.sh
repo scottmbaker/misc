79cce23b6155c1d6607d5743a24a0869a3477803
Scott Baker <smbaker@gmail.com>
2018-05-04 13:05:46 -0700

    tosca script

---
+++ xos/helm-tosca.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+TOSCA_URL=`minikube service xos-gui --url`
+ACCOUNT=admin@opencord.org
+PASSWORD=letmein
+RECIPE=$1
+
+echo "Tosca URL is $TOSCA_URL"
+curl -H "xos-username: $ACCOUNT" -H "xos-password: $PASSWORD" -X POST --data-binary @RECIPE $TOSCA_URL/run
