cd824c435036a6927449b9bdc420834806b1883c
Scott Baker <smbaker@gmail.com>
2018-05-09 16:21:00 -0700

    support driver=none

--- xos/helm-cycle.sh
+++ xos/helm-cycle.sh
@@ -1,23 +1,34 @@
 #! /bin/bash
 
-helm delete rcord-lite
+USE_PROFILE=${PROFILE:-rcord-lite}
+
+echo "Profile is $USE_PROFILE"
+
+helm delete $USE_PROFILE
 helm delete xos-core
-helm del --purge rcord-lite
+helm delete xossh
+helm del --purge $USE_PROFILE
 helm del --purge xos-core
+helm del --purge xossh
+
+kubectl delete pods --all
+kubectl delete configmaps --all
 
 GET_PODS_RESULT=`kubectl get pods`
 while [ -n "$GET_PODS_RESULT" ]; do
    GET_PODS_RESULT=`kubectl get pods`
 done
 
-kubectl delete pods --all
-kubectl delete configmaps --all
-
 set -e
 cd ~/cord/build
-eval $(minikube docker-env)
+
+docker_env=`minikube docker-env`
+if [[ "$docker_env" != "'none' driver does not support 'minikube docker-env' command" ]]; then
+  eval $docker_env
+fi
+
 scripts/imagebuilder.py -f helm-charts/examples/filter-images.yaml
 cd helm-charts
 helm install xos-core -n xos-core -f examples/candidate-tag-values.yaml
-helm dep update xos-profiles/rcord-lite
-helm install xos-profiles/rcord-lite -n rcord-lite -f examples/candidate-tag-values.yaml
+helm dep update xos-profiles/$USE_PROFILE
+helm install xos-profiles/$USE_PROFILE -n $USE_PROFILE -f examples/candidate-tag-values.yaml
