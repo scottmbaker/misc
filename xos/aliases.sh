31c94220802774f36b308faaa56d7acccbed1acb
Scott Baker <smbaker@gmail.com>
2018-05-14 09:03:47 -0700

    helm-charts moved

--- xos/aliases.sh
+++ xos/aliases.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 alias helm-cycle="/opt/smbaker/helm-cycle.sh"
-alias helm-xossh-start="kubectl get pods | grep -i xossh | grep -v "erminating" > /dev/null || (pushd ~/cord/build/helm-charts; helm install xos-tools/xossh -n xossh; popd)"
+alias helm-xossh-start="kubectl get pods | grep -i xossh | grep -v "erminating" > /dev/null || (pushd ~/cord/helm-charts; helm install xos-tools/xossh -n xossh; popd)"
 alias helm-xossh-stop="helm del --purge xossh"
-alias helm-xossh="~/cord/build/helm-charts/xos-tools/xossh/xossh-attach.sh"
+alias helm-xossh="~/cord/helm-charts/xos-tools/xossh/xossh-attach.sh"
 alias helm-wait="/opt/smbaker/helm-wait.sh"
 alias helm-tosca="/opt/smbaker/helm-tosca.sh"
