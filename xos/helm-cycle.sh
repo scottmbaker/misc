4ba8dc765a2582dcecaf20172cc2e1f282c9f5ac
Scott Baker <smbaker@gmail.com>
2018-05-30 08:37:53 -0700

    restart onos

--- xos/helm-cycle.sh
+++ xos/helm-cycle.sh
@@ -11,6 +11,7 @@ helm del --purge $USE_PROFILE
 helm del --purge xos-core
 helm del --purge xossh
 helm del --purge cord-kafka
+helm del --purge onos-cord
 
 kubectl delete pods --all
 kubectl delete configmaps --all
@@ -36,13 +37,20 @@ fi
 scripts/imagebuilder.py -f ~/cord/helm-charts/examples/filter-images.yaml
 
 cd ~/cord/helm-charts
+
+helm upgrade --install onos-cord ./onos
+~/openstack-helm/tools/deployment/common/wait-for-pods.sh default
+ONOS_CORD_POD=$(kubectl get pods | grep onos-cord | awk '{print $1}')
+kubectl cp ~/.ssh/id_rsa "$ONOS_CORD_POD":/root/node_key
+
 helm dep update xos-core
 helm install xos-core -n xos-core \
+     -f examples/image-tag-candidate.yaml -f examples/imagePullPolicy-IfNotPresent.yaml
+helm dep update xos-profiles/$USE_PROFILE
+helm install xos-profiles/$USE_PROFILE -n $USE_PROFILE \
      --set computeNodes.master.name="$( hostname )" \
      --set vtn-service.sshUser="$( whoami )" \
      -f examples/image-tag-candidate.yaml -f examples/imagePullPolicy-IfNotPresent.yaml
-helm dep update xos-profiles/$USE_PROFILE
-helm install xos-profiles/$USE_PROFILE -n $USE_PROFILE -f examples/image-tag-candidate.yaml -f examples/imagePullPolicy-IfNotPresent.yaml
 
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
 helm install --name cord-kafka --set replicas=1 incubator/kafka
