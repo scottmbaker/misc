d78b4372f981fefc29c44a4a6e9ce77afdc9ee76
Scott Baker <smbaker@gmail.com>
2018-06-05 08:49:35 -0700

    pull more prereqs

--- xos/pull-prereqs.sh
+++ xos/pull-prereqs.sh
@@ -3,3 +3,11 @@
 set -e
 sudo docker pull xosproject/xos-base:master
 sudo docker tag xosproject/xos-base:master xosproject/xos-base:candidate
+sudo docker pull xosproject/xos-tosca:master
+sudo docker tag xosproject/xos-tosca:master xosproject/xos-tosca:candidate
+sudo docker pull xosproject/chameleon:master
+sudo docker tag xosproject/chameleon:master xosproject/chameleon:candidate
+sudo docker pull xosproject/xos-gui:master
+sudo docker tag xosproject/xos-gui:master xosproject/xos-gui:candidate
+sudo docker pull xosproject/xos-ws:master
+sudo docker tag xosproject/xos-ws:master xosproject/xos-ws:candidate
