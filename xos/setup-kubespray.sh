5b170b9cf150cd4e07b9236fcc42a5beef5dc43d
Scott Baker <smbaker@gmail.com>
2018-07-13 10:07:40 -0700

    add setup-kubespray script

---
+++ xos/setup-kubespray.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+git clone https://github.com/zdw/cloudlab_bootstrap
+cd cloudlab_bootstrap/ks_install_tester
+./test_boostrap.sh
+vagrant up
+vagrant ssh-config >> ~/.ssh/config
+vagrant ssh-config | sed -e 's/Host k8s-0/Host 10.90.0.10/g' >> ~/.ssh/config
+cd ~/cord/automation-tools/kubespray-installer
+REMOTE_SSH_USER="vagrant" ./setup.sh -i test
