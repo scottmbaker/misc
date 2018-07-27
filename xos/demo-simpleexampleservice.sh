99cc5f62a9c356412ff16520a731176cb9dcf969
Scott Baker <smbaker@gmail.com>
2018-07-27 09:54:52 -0700

    demo-simpleexampleservice script

---
+++ xos/demo-simpleexampleservice.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -e 
+pushd ~/cord/helm-charts
+helm dep update xos-profiles/demo-simpleexampleservice
+helm install xos-profiles/demo-simpleexampleservice -n demo-simpleexampleservice \
+     -f examples/image-tag-candidate.yaml -f examples/imagePullPolicy-IfNotPresent.yaml
+
+SIMPLEEXAMPLESERVICE_PATH=~/cord/orchestration/xos_services/simpleexampleservice
+USERNAME=admin@opencord.org
+PASSWORD=letmein
+TOSCA_URL=http://$( hostname ):30007
+TOSCA_FN=$SIMPLEEXAMPLESERVICE_PATH/xos/examples/SimpleExampleServiceInstance.yaml
+curl -H "xos-username: $USERNAME" -H "xos-password: $PASSWORD" -X POST --data-binary @$TOSCA_FN $TOSCA_URL/run
+popd
