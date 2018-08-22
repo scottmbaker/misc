4c80b18bb24ac3286478608385a69bb97926f517
Scott Baker <smbaker@gmail.com>
2018-08-22 13:27:56 -0700

    fabric

--- xos/helm-onos.sh
+++ xos/helm-onos.sh
@@ -1,2 +1,2 @@
-ONOS_ADDR=`kubectl get services | grep -i onos-cord-ssh | awk '{print $3}'`
+ONOS_ADDR=`kubectl get services | grep -i onos-fabric-ssh | awk '{print $3}'`
 ssh karaf@$ONOS_ADDR -p 8101
