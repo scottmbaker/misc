6c0f124e696f43335ba029ce7f0c3f9ef9fb2e3b
Scott Baker <smbaker@gmail.com>
2019-02-04 09:40:58 -0800

    build-and-push-sync script

--- xos/rebuild-local.sh
+++ xos/rebuild-local.sh
@@ -2,7 +2,6 @@
 set -e
 ALL_CONTAINERS="xos-libraries xos-client xos-synchronizer-base xos-core chameleon xos-tosca onos-synchronizer vtn-synchronizer openstack-synchronizer exampleservice-synchronizer addressmanager-synchronizer kubernetes-synchronizer tosca-loader volt-synchronizer fabric-synchronizer fabric-crossconnect-synchronizer vrouter-synchronizer vsg-hw-synchronized rcord-synchronizer hippie-oss-synchronizer att-workflow-driver-synchronizer"
 CONTAINERS=${1:-$ALL_CONTAINERS}
-echo $CONTAINERS
 CORD_BASE=~/projects/opencord
 cd $CORD_BASE/orchestration/xos
 if [[ $CONTAINERS == *"chameleon"* ]]; then
