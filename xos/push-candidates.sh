d5690d6a018bc15fad58b82fc1376d41445688d8
Scott Baker <smbaker@gmail.com>
2019-02-06 21:12:34 -0800

    simpleexampleservice was missing

--- xos/push-candidates.sh
+++ xos/push-candidates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-ALL_CONTAINERS="xos-libraries xos-client xos-synchronizer-base xos-core chameleon xos-tosca onos-synchronizer vtn-synchronizer openstack-synchronizer exampleservice-synchronizer addressmanager-synchronizer kubernetes-synchronizer tosca-loader volt-synchronizer fabric-synchronizer fabric-crossconnect-synchronizer vrouter-synchronizer vsg-hw-synchronized rcord-synchronizer hippie-oss-synchronizer att-workflow-driver-synchronizer mcord-synchronizer progran-synchronizer"
+ALL_CONTAINERS="xos-libraries xos-client xos-synchronizer-base xos-core chameleon xos-tosca onos-synchronizer vtn-synchronizer openstack-synchronizer exampleservice-synchronizer addressmanager-synchronizer kubernetes-synchronizer tosca-loader volt-synchronizer fabric-synchronizer fabric-crossconnect-synchronizer vrouter-synchronizer vsg-hw-synchronized rcord-synchronizer hippie-oss-synchronizer att-workflow-driver-synchronizer mcord-synchronizer progran-synchronizer addressmanager-synchronizer simpleexampleservice-synchronizer"
 CONTAINERS=${1:-$ALL_CONTAINERS}
 
 TAG=test
-REPOS="xos-core xos-client xos-libraries xos-synchronizer-base att-workflow-driver-synchronizer rcord-synchronizer fabric-crossconnect-synchronizer fabric-synchronizer volt-synchronizer tosca-loader kubernetes-synchronizer addressmanager-synchronizer simpleexampleservice-synchronizer exampleservice-synchronizer openstack-synchronizer onos-synchronizer vrouter-synchronizer xos-tosca"
+REPOS="xos-core xos-client xos-libraries xos-synchronizer-base att-workflow-driver-synchronizer rcord-synchronizer fabric-crossconnect-synchronizer fabric-synchronizer volt-synchronizer tosca-loader kubernetes-synchronizer addressmanager-synchronizer simpleexampleservice-synchronizer exampleservice-synchronizer openstack-synchronizer onos-synchronizer vrouter-synchronizer xos-tosca mcord-synchronizer progran-synchronizer addressmanager-synchronizer"
 
 for REPO in $REPOS; do
   if [[ $CONTAINERS == *"$REPO"* ]]; then
