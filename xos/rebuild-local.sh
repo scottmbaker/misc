ba301dd8f8ac68d4e9ae3c5794d69f6924e3f823
Scott Baker <smbaker@gmail.com>
2019-02-28 11:53:14 -0800

    forgot ses

--- xos/rebuild-local.sh
+++ xos/rebuild-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-ALL_CONTAINERS="xos-libraries xos-client xos-synchronizer-base xos-core chameleon xos-tosca onos-synchronizer vtn-synchronizer openstack-synchronizer exampleservice-synchronizer addressmanager-synchronizer kubernetes-synchronizer tosca-loader volt-synchronizer fabric-synchronizer fabric-crossconnect-synchronizer vrouter-synchronizer vsg-hw-synchronized rcord-synchronizer hippie-oss-synchronizer att-workflow-driver-synchronizer"
+ALL_CONTAINERS="xos-libraries xos-client xos-synchronizer-base xos-core chameleon xos-tosca onos-synchronizer vtn-synchronizer openstack-synchronizer exampleservice-synchronizer addressmanager-synchronizer kubernetes-synchronizer tosca-loader volt-synchronizer fabric-synchronizer fabric-crossconnect-synchronizer vrouter-synchronizer vsg-hw-synchronized rcord-synchronizer hippie-oss-synchronizer att-workflow-driver-synchronizer simpleexampleservice-synchronizer"
 CONTAINERS=${1:-$ALL_CONTAINERS}
 CORD_BASE=${CORD_DIR:-~/projects/opencord}
 cd $CORD_BASE/orchestration/xos
