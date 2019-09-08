776d5ada004ed423f394bdea9387bbf816245e5e
Scott Baker <smbaker@gmail.com>
2019-09-08 15:51:36 -0700

    new stuff

--- xos/aliases.sh
+++ xos/aliases.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+alias cdgr="cd ~/projects/gopath/src/github.com/opencord"
 alias helm-cycle="/opt/smbaker/helm-cycle.sh"
 alias helm-xossh-start="kubectl get pods | grep -i xossh | grep -v "erminating" > /dev/null || (pushd ~/cord/helm-charts; helm install xos-tools/xossh -f examples/xossh-candidate.yaml -n xossh; popd)"
 alias helm-xossh-stop="helm del --purge xossh"
@@ -13,3 +14,8 @@ alias dockerfile-candidates="python /opt/smbaker/dockerfile-candidates.py"
 alias dockerfile-VERSION="bash /opt/smbaker/dockerfile-VERSION.sh"
 alias rebuild-local="bash /opt/smbaker/rebuild-local.sh"
 alias push-candidates="bash /opt/smbaker/push-candidates.sh"
+
+dosfix() {
+    echo fixing $1
+    sed -i 's/\r//' $1 && sed -i 's/[ \t]*$//' $1
+}
