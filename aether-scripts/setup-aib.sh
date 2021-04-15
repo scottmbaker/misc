1f3a5793bdc50ac90c7066c181ef6d1813a2abb7
Scott Baker <smbaker@gmail.com>
2021-04-15 10:19:20 -0700

    latest scripts

--- aether-scripts/setup-aib.sh
+++ aether-scripts/setup-aib.sh
@@ -12,6 +12,8 @@ if [ -z "$GERRITPW" ]; then
     exit
 fi
 
+sudo apt-get install -y git ethtool
+
 sudo apt-get update && sudo apt-get install -y python-pip
 #pip install --upgrade --user pip
 sudo python -m pip install --upgrade pip==19.3.1
@@ -23,19 +25,19 @@ cd aether-in-a-box
 sed -i 's/\xC2\xA0/ /g' Makefile
 make /tmp/build/milestones/k8s-ready /tmp/build/milestones/helm-ready
 
-mkdir -p /users/smbaker/cord
-cd /users/smbaker/cord
+mkdir -p $HOME/cord
+cd $HOME/cord
 git clone https://${GERRITUSER}:${GERRITPW}@gerrit.opencord.org/a/aether-helm-charts
-cd /users/smbaker/cord/aether-helm-charts/omec
+cd $HOME/cord/aether-helm-charts/omec
 helm dep update omec-control-plane
 
-cd /users/smbaker/cord
+cd $HOME/cord
 git clone https://${GERRITUSER}:${GERRITPW}@gerrit.opencord.org/a/helm-charts
 cd helm-charts
 
-cd /users/smbaker
+cd $HOME
 git clone https://github.com/sbconsulting/misc
 sudo scp misc/aether-scripts/* /usr/bin/
 
-cd /users/smbaker
+cd $HOME
 git clone https://github.com/sbconsulting/q3demo
